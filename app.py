c08f0d46a70ac620bb661dfcc56b7ba4f5a859f8
Rabih-Tahouf <rabihtahouf@Rabihs-Air.fios-router.home>
2021-02-17 22:02:01 -0500

    Fixed internal server error.

--- app.py
+++ app.py
@@ -10,16 +10,11 @@ from resources.store import Store, StoreList
 from db import db 
 
 
-app = Flask(__name__)
-db.init_app(app) 
+app = Flask(__name__) 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.enivron.get('DATABASE_URL','sqlite:///data.db')#sqlal db will live at root folder of our porject; can be Mysql, postgreSQL or any other sql enginer
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #turning off "flask sqlal tracker" b/c sqlal has its own modification tracker
 app.secret_key = 'Rabih'
 api = Api(app)
-
-@app.before_first_request  #decorator, run it (method) before first request into this app/ will create data.db file, create tables into this file unless they exist
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
---
+++ run.py
@@ -0,0 +1,8 @@
+from app impport init_app
+from db import db
+
+db.init_app(app)
+
+@app.before_first_request  #decorator, run it (method) before first request into this app/ will create data.db file, create tables into this file unless they exist
+def create_tables():
+    db.create_all()
